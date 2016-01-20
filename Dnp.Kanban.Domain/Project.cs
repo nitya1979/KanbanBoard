0157e910dbab6860f9fc22095d2a1c17f2adb7b5
nitya1979 <nityaprakash@gmail.com>
2016-01-19 23:08:14 -0500

    Added Domain

---
+++ Dnp.Kanban.Domain/Project.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnp.Kanban.Domain
+{
+    class Project
+    {
+    }
+}
