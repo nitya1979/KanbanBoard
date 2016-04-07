d8c00e81663aa0609b696efae4efd86cac1781d1
nitya <nitya@DESKTOP-KHN0LFM>
2016-04-07 06:44:05 -0400

    Removed ViewModel project

--- Dnp.Kanban.ViewModel/Project.cs
+++ Dnp.Kanban.ViewModel/Project.cs
@@ -9,9 +9,20 @@ namespace Dnp.Kanban.ViewModel
     {
         public int ProjectID { get; set; }
 
+        [Required(ErrorMessage = "Please enter name of the project.")]
+        [MaxLength(150)]
         public string Name { get; set; }
 
-        
+        [MaxLength(400)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Invalid start date")]
+        [Required(ErrorMessage = "Please enter start date.")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Invalid end date.")]
+        [Required(ErrorMessage = "Please enter end date.")]
+        public DateTime EndDate { get; set; }
 
     }
 }
