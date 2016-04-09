ff42193d2fea217d98455c28dfb2f736109d492d
nitya <nitya@DESKTOP-KHN0LFM>
2016-04-09 18:56:32 -0400

    Added Mapper to web project to map Domain and ViewModel

---
+++ Dnp.Kanban.ViewModel/ProjectViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dnp.Kanban.ViewModel
+{
+    public class ProjectViewModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Please enter name of the project.")]
+        [MaxLength(150)]
+        public string Name { get; set; }
+
+        [MaxLength(400)]
+        public string Description { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Invalid start date")]
+        [Required(ErrorMessage = "Please enter start date.")]
+        public DateTime StartDate { get; set; }
+
+        [DataType(DataType.Date, ErrorMessage = "Invalid end date.")]
+        [Required(ErrorMessage = "Please enter end date.")]
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ Dnp.Kanban.Web/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Dnp.Kanban.Domain;
+using Dnp.Kanban.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dnp.Kanban.Web
+{
+    public static class AutoMapperConfig
+    {
+        public static void Setup()
+        {
+            Mapper.CreateMap<ProjectViewModel, Project>();
+            Mapper.CreateMap<Project, ProjectViewModel>();
+
+        }
+    }
+}
