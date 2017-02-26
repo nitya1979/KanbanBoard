a6ea8e8654062ee359658157d46d6b1182f84f4b
nityaprakash <nityaprakash@NITYAPRAKASF402>
2017-02-26 14:54:41 -0500

    Add NUnit test

--- Dnp.Kanban.DomainTests/ProjectServiceTests.cs
+++ Dnp.Kanban.DomainTests/ProjectServiceTests.cs
@@ -1,20 +1,78 @@
-﻿using NUnit.Framework;
-using Dnp.Kanban.Domain;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Dnp.Kanban.Domain.Tests
 {
-    [TestFixture()]
+    [TestFixture]
     public class ProjectServiceTests
     {
-        [Test()]
-        public void SaveStageTest()
+        Mock<IProjectRepository> mockRepository;
+
+        [SetUp]
+        public void Setup()
         {
-            Assert.Fail();
+            mockRepository = new Mock<IProjectRepository>();
+
+            mockRepository.Setup(r => r.GetProjectStages(1)).Returns(() =>
+            {
+                return new List<ProjectStage> { new ProjectStage {  ID = 1, ProjectID = 1, StageName ="Back Log"},
+                                                new ProjectStage {  ID = 2, ProjectID = 1, StageName ="In Progress"},
+                                                new ProjectStage {  ID = 3, ProjectID = 1, StageName ="Completed"}
+                                                };
+            });
+        }
+
+        [Test]
+        public void SaveStageWithExistingNameFailTest()
+        {
+
+            ProjectStage stageMock = new ProjectStage { ProjectID = 1, StageName = "Back Log" };
+
+            mockRepository.Setup(r => r.SaveStage(stageMock)).ReturnsAsync(5);
+
+            ProjectService service = new ProjectService(mockRepository.Object);
+
+            TestDelegate testDel = ()=> { service.SaveStage(stageMock); };
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(  testDel);
+
+            Assert.AreEqual(string.Format("Stage '{0}' already exists", stageMock.StageName), exception.Message, "Not sent correct message");
+
+
+        }
+
+        [Test]
+        public void SaveStageWithNewNameTest()
+        {
+
+            ProjectStage stageMock = new ProjectStage { ProjectID = 1, StageName = "Done" };
+
+            mockRepository.Setup(r => r.SaveStage(stageMock)).ReturnsAsync(4);
+
+            ProjectService service = new ProjectService(mockRepository.Object);
+
+            int newValue = service.SaveStage(stageMock).Result;
+
+            Assert.IsTrue(newValue > 0, "New Stage save is failed");
+
+        }
+
+        [Test]
+        public void SaveStageWithProjectIdZeroFailTest()
+        {
+            ProjectStage mockStage = new ProjectStage { StageName = "New Stage" };
+
+            mockRepository.Setup(r => r.SaveStage(mockStage)).ReturnsAsync(4);
+
+            ProjectService service = new ProjectService(mockRepository.Object);
+
+            InvalidOperationException exception = Assert.Throws<InvalidOperationException>(() => { service.SaveStage(mockStage); });
+
+            Assert.AreEqual("Project ID must be defined.", exception.Message, "Incorrect message for error");
+
+            
         }
     }
 }
